872eca4bc686a22f5b8f92e0faa17538e7b2f391
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 05:46:03 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species)
     FROM characters
     GROUP BY characters.species 
-    ORDER BY characters.species DESC LIMIT 1"
+    ORDER BY characters.species DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
