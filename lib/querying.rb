0440b63ad059599f4a858b95d8739a068a375092
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 05:55:24 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,7 +29,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title 
     FROM series
-    GROUP BY series.cha"
+    JOIN books
+    WHERE series.id = books.series_id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
