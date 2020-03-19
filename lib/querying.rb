34b6e04de5a7d0116989089158d7d288def1cf37
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 05:41:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,7 +1,7 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, book.year
     FROM books 
-    WHERE book.serie_id =1 
+    WHERE books.serie_id =1 
     ORDER BY year;"
 end
 
