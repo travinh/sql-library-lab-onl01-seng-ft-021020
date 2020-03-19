b90f469024fe49eda6796fdd3f94cbe0a675dfe4
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 06:01:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title 
     FROM series
     JOIN books, character_books, characters
-    ON series.id = books.series_id and book.id = character_books.book_id and character_books.character_id = characters.id
+    ON series.id = books.series_id and book.id = character_books.books_id and character_books.character_id = characters.id
     WHERE characters.species = \"human\"
     GROUP BY series.title 
     ORDER BY COUNT(characters.species) DESC LIMIT 1;"
