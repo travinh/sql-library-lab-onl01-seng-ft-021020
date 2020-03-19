43bf365a9ccc810f6e6f3a1e6975b250365d75c0
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-03-19 06:00:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
     FROM authors
     JOIN subgenres, series
-    WHERE authors.id = series.author_id and subgenres.id = series.subgenre_id;"
+    ON authors.id = series.author_id and subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
